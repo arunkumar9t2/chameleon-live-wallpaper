12db787a50c50de61f6e54139f8ba47cbdbe9f32
Arunkumar <arunk.beece@gmail.com>
2017-04-12 01:13:25 +0530

    Setup data binding and setup app theme.

--- app/src/main/java/arun/com/chameleonskinforkwlp/MainActivity.java
+++ app/src/main/java/arun/com/chameleonskinforkwlp/MainActivity.java
@@ -1,22 +1,25 @@
 package arun.com.chameleonskinforkwlp;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import arun.com.chameleonskinforkwlp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(mainBinding.toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
