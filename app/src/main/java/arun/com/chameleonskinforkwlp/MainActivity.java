0f217c94e7a781407deef640d2c371a1fff0e422
Arunkumar <arunk.beece@gmail.com>
2017-04-13 01:39:52 +0530

    Side drawer implementation.

--- app/src/main/java/arun/com/chameleonskinforkwlp/MainActivity.java
+++ app/src/main/java/arun/com/chameleonskinforkwlp/MainActivity.java
@@ -1,6 +1,7 @@
 package arun.com.chameleonskinforkwlp;
 
 import android.Manifest;
+import android.app.WallpaperManager;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.ComponentName;
@@ -19,9 +20,18 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import java.io.File;
 import java.io.IOException;
@@ -29,6 +39,8 @@ import java.io.IOException;
 import arun.com.chameleonskinforkwlp.databinding.ActivityMainBinding;
 import arun.com.chameleonskinforkwlp.engine.ExtractorService;
 import arun.com.chameleonskinforkwlp.preferences.Preferences;
+import arun.com.chameleonskinforkwlp.util.Util;
+import arun.com.chameleonskinforkwlp.wallpaper.ChameleonWallpaperService;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,6 +64,62 @@ public class MainActivity extends AppCompatActivity {
     private void initToolbar() {
         setSupportActionBar(binding.toolbar);
         Glide.with(this).load(R.drawable.backdrop).centerCrop().into(binding.backdrop);
+        AccountHeader header = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.backdrop)
+                .withHeaderBackgroundScaleType(ImageView.ScaleType.CENTER_CROP)
+                .withCompactStyle(true)
+                .build();
+        new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(binding.toolbar)
+                .withAccountHeader(header)
+                .withSelectedItem(-1)
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                                .withName(R.string.set_as_system_wallpaper)
+                                .withIdentifier(1)
+                                .withSelectable(false)
+                                .withIcon(GoogleMaterial.Icon.gmd_wallpaper),
+                        new PrimaryDrawerItem().withName(R.string.use_with_kustom)
+                                .withSelectable(false)
+                                .withIcon(GoogleMaterial.Icon.gmd_palette)
+                                .withIdentifier(2),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.rate_this_app)
+                                .withSelectable(false)
+                                .withIdentifier(3)
+                                .withIcon(GoogleMaterial.Icon.gmd_star),
+                        new DividerDrawerItem()
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null) {
+                            switch (drawerItem.getIdentifier()) {
+                                case 1:
+                                    final Intent setWallpaper = new Intent();
+                                    setWallpaper.setAction(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+                                    setWallpaper.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(getPackageName(), ChameleonWallpaperService.class.getName()));
+                                    startActivityForResult(setWallpaper, 0);
+                                    break;
+                                case 2:
+                                    if (Util.isPackageInstalled(MainActivity.this, "org.kustom.wallpaper")) {
+                                        Util.openApp(MainActivity.this, "org.kustom.wallpaper");
+                                    } else {
+                                        Snackbar.make(binding.coordinatorLayout, R.string.kustom_not_found, Snackbar.LENGTH_SHORT).show();
+                                    }
+                                    break;
+                                case 3:
+                                    Util.openPlayStore(MainActivity.this, getPackageName());
+                                    break;
+                            }
+                        }
+                        return false;
+                    }
+                })
+                .build();
     }
 
     private void initThemeSelector() {
--- app/src/main/java/arun/com/chameleonskinforkwlp/util/Util.java
+++ app/src/main/java/arun/com/chameleonskinforkwlp/util/Util.java
@@ -1,11 +1,47 @@
 package arun.com.chameleonskinforkwlp.util;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.databinding.BindingConversion;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 public class Util {
     @BindingConversion
     public static ColorDrawable convertColorToDrawable(int color) {
         return color != 0 ? new ColorDrawable(color) : null;
+    }
+
+    public static boolean isPackageInstalled(@NonNull Context c, @Nullable String pkgName) {
+        if (pkgName == null) return false;
+
+        PackageManager pm = c.getApplicationContext().getPackageManager();
+        try {
+            pm.getPackageInfo(pkgName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static void openPlayStore(@NonNull Context context, @NonNull String appPackageName) {
+        try {
+            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
+    }
+
+    public static void openApp(Activity activity, String packageName) {
+        if (isPackageInstalled(activity, packageName)) {
+            final Intent intent = activity.getPackageManager().getLaunchIntentForPackage(packageName);
+            if (intent != null) {
+                activity.startActivity(intent);
+            }
+        }
     }
 }
